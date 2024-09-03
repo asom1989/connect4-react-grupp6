2e3a1b676e0fb6fb226d08b2fbd2f97962391379
Staffan Björk <staffanbjork86@gmail.com>
2024-09-03 18:11:34 +0200

    Moves class - moveIsValid method added

--- src/classes/Moves.ts
+++ src/classes/Moves.ts
@@ -7,4 +7,13 @@ export default class Moves {
     this.moveMade = 0;
     this.lastMove = undefined!;
   }
+
+  moveIsValid(board: Matrix, col: number) {
+    for (let row = BoardProps.Rows - 1; row >= 0; row--){
+      if (!board[row][col]) {
+        return { row, col };
+      }
+    }
+    return null;
+  }
 }
