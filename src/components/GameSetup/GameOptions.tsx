cbfe6a4eb31900050863401a7246a5a5cf5f9d34
asom1989 <asom.1989@yahoo.com>
2024-09-04 14:36:48 +0200

    Dela upp GameSetup-komponenten i mindre komponenter

---
+++ src/components/GameSetup/GameOptions.tsx
@@ -0,0 +1,26 @@
+import { GameType } from "../../types/types";
+
+interface GameOptionsProps {
+  setGameType: (gameType: GameType) => void;
+}
+
+const GameOptions = ({ setGameType }: GameOptionsProps) => {
+  return (
+    <section className="game-options">
+      <h1 className="game-title">Connect-4 Game</h1>
+      <div className="buttons-wrapper">
+        <button type="button" onClick={() => setGameType(GameType.Dual)}>
+          1 VS 1
+        </button>
+        <button type="button" onClick={() => setGameType(GameType.Single)}>
+          1 player
+        </button>
+        <button type="button" onClick={() => setGameType(GameType.AI)}>
+          Cpu VS Cpu
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default GameOptions;
--- src/components/GameSetup/GameSetup.tsx
+++ src/components/GameSetup/GameSetup.tsx
@@ -1,27 +1,15 @@
 import { useState } from "react";
 import { GameType } from "../../types/types";
 import "./game-setup.css";
+import GameOptions from "./GameOptions";
 const GameSetup = () => {
   const [gameType, setGameType] = useState<GameType>(GameType.Dual);
 
   return (
-    <>
-      <section className="game-options">
-        <h1>Connect-4 Game</h1>
-        <div className="buttons-wrapper">
-          <button type="button" onClick={() => setGameType(GameType.Dual)}>
-            1 VS 1
-          </button>
-          <button type="button" onClick={() => setGameType(GameType.Single)}>
-            1 player
-          </button>
-          <button type="button" onClick={() => setGameType(GameType.AI)}>
-            Cpu VS Cpu
-          </button>
-        </div>
-        <h1>{gameType}</h1>
-      </section>
-    </>
+    <article className="game-setup">
+      <h1>{gameType}</h1>
+      <GameOptions setGameType={setGameType} />
+    </article>
   );
 };
 
