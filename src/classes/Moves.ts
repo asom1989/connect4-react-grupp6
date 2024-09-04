89584dd0c1402575284df3026b993e27bdbf876c
Staffan Björk <staffanbjork86@gmail.com>
2024-09-04 12:56:09 +0200

    Moves class - columnStatus prop added, type import updated

--- src/classes/Moves.ts
+++ src/classes/Moves.ts
@@ -1,15 +1,17 @@
-import { Move, Matrix, BoardProps, PlayerType, Color } from "../types/types";
+import { Move, Matrix, BoardProps, PlayerType, Color, ColumnStatus } from "../types/types";
 export default class Moves {
   movesMade: number;
+  columnStatus: ColumnStatus;
   lastMove: Move;
 
   constructor() {
     this.movesMade = 0;
+    this.columnStatus = new Array(BoardProps.Cols).fill(6);
     this.lastMove = undefined!;
   }
 
   moveIsValid(board: Matrix, col: number) {
-    for (let row = BoardProps.Rows - 1; row >= 0; row--){
+    for (let row = BoardProps.Rows - 1; row >= 0; row--) {
       if (!board[row][col]) {
         return { row, col };
       }
@@ -26,10 +28,12 @@ export default class Moves {
   }
 
   makeMove(board: Matrix, playerType: PlayerType, currentPlayer: number, _move: Move) {
-    if (this.movesMade >= 42) { return }
+    if (this.movesMade >= 42) {
+      return;
+    }
 
     const validMove = playerType === 1 ? _move : this.computerEasyMove(board);
-    
+
     if (validMove) {
       board[validMove.row][validMove.col] = Color[currentPlayer];
       this.lastMove = validMove;
