666149224d9e286206f337fed01675a90745ac2f
Linus Tropp <linustropp@gmail.com>
2024-09-09 16:42:47 +0200

    added board.css and drop effect

--- src/classes/Board.tsx
+++ src/classes/Board.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BoardProps, BoardState, Setup } from "../types/types";
 import Moves from "./Moves";
 import Player from "./Player";
+import "./board.css";
 
 interface BoardPropsPlayer {
   onQuit: () => void;
@@ -23,7 +24,6 @@ export default class Board extends React.Component<
 
     // initialize Player
     const player = props.gameState;
-    //this.player = new Player(player.name, player.playerType, player.playerColor);
     this.playerOne = new Player(player.playerOneName, player.playerOneType, 1);
     this.playerTwo = new Player(player.playerTwoName, player.playerTwoType, 0);
 
@@ -35,6 +35,10 @@ export default class Board extends React.Component<
 
     this.resetGame = this.resetGame.bind(this);
   }
+
+  colorToString = (color: number): string => {
+    return color === 1 ? "red" : "yellow";
+  };
 
   resetGame = () => {
     this.moves = new Moves();
@@ -49,23 +53,41 @@ export default class Board extends React.Component<
       .fill(null)
       .map(() => Array(BoardProps.Cols).fill(null));
   }
-
+  
   handlePlayerMove(columnIndex: number) {
     const { currentPlayer } = this.state;
     if (this.moves.columnStatus[columnIndex] <= 0) return;
 
-    this.moves.makeMove(this.state.matrix, 1, currentPlayer.color, columnIndex);
+    const validMove = this.moves.getMovePosition(this.state.matrix, columnIndex);
+    if (!validMove) return;
+
     const newMatrix = this.state.matrix.map((row) => row.slice());
+    this.moves.makeMove(newMatrix, 1, currentPlayer.color, columnIndex);
+
+    this.setState({ matrix: newMatrix }, () => {
+      const columnElement = document.querySelector(
+        `.row-${validMove.row}.col-${validMove.col}`
+      ) as HTMLElement;
+
+      if (columnElement) {
+        columnElement.classList.remove('red', 'yellow');
+
+        columnElement.classList.add('animate');
+
+        setTimeout(() => {
+          columnElement.classList.add(this.colorToString(currentPlayer.color));
+        }, 0);
+      }
+    });
 
     this.setState((prevState) => ({
-      matrix: newMatrix,
       currentPlayer:
         prevState.currentPlayer === this.playerOne
           ? this.playerTwo
           : this.playerOne,
     }));
   }
-
+  
   render() {
     const { currentPlayer } = this.state;
     return (
@@ -83,10 +105,10 @@ export default class Board extends React.Component<
               {row.map((column, columnIndex) => (
                 <div
                   key={columnIndex}
-                  className={`column ${column || ""}`}
+                  className={`column row-${rowIndex} col-${columnIndex} ${column || ""}`}
                   onClick={() => this.handlePlayerMove(columnIndex)}
                   style={{
-                    backgroundColor: column || "#D9D9D9", // set column background color
+                    backgroundColor: "white", // set column background color
                   }}
                 ></div>
               ))}
