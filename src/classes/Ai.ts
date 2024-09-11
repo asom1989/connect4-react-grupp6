f20ffaf5ad9304e90760a7a29d6e80db2e7d030a
Staffan Björk <staffanbjork86@gmail.com>
2024-09-11 12:05:35 +0200

    Ai class - movesMade prop removed

--- src/classes/Ai.ts
+++ src/classes/Ai.ts
@@ -4,19 +4,16 @@ export default class Ai {
   ROWS: number;
   COLS: number;
   currentPlayer: Color;
-  movesMade: number;
 
   constructor() {
     this.ROWS = BoardProps.Rows;
     this.COLS = BoardProps.Cols;
     this.currentPlayer = 1;
-    this.movesMade = 0;
   }
 
   makeMove(board: Matrix, move: number, player: string) {
     for (let row = this.ROWS - 1; row >= 0; row--) {
       if (!board[row][move]) {
-        this.movesMade++;
         board[row][move] = player;
         break;
       }
@@ -27,7 +24,6 @@ export default class Ai {
   undoMove(board: Matrix, move: number) {
     for (let row = 0; row < this.ROWS; row++) {
       if (board[row][move] !== null) {
-        this.movesMade--;
         board[row][move] = null;
         break;
       }
