d4de2adf0d1cb6b696d3d7cceee3a3445d781cc9
asom1989 <asom.1989@yahoo.com>
2024-09-11 13:24:23 +0200

    change the olayer class to receive the player image

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -4,10 +4,12 @@ export default class Player {
   name: string;
   type: PlayerType;
   color: Color;
+  avatar: string;
 
-  constructor(name: string, type: number, color: number) {
+  constructor(name: string, type: number, color: number, avatar: string) {
     this.name = name;
     this.type = type;
     this.color = color;
+    this.avatar = avatar || "/public/images/user_icon_001.jpg";
   }
 }
