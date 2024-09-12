722ac729b3c9dcc6cffe3bc7e2ff54783507bb30
Staffan Björk <staffanbjork86@gmail.com>
2024-09-12 19:57:45 +0200

    Backend - userService.ts - login function added

--- server/src/service/userService.ts
+++ server/src/service/userService.ts
@@ -1,7 +1,10 @@
 import crypto from "crypto";
+import fs from "fs";
+import { Request, Response } from "express";
 
 
 const SALT = "2cUMi+VRsGjxqank5yixHA==";
+const USERNAMEANDPASSWORDREGEX = /\w{1,15}/;
 
 function getHash(userData: string) {
   const hash = crypto
@@ -9,3 +12,22 @@ function getHash(userData: string) {
     .toString("hex");
   return hash;
 }
+
+
+const login = (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!USERNAMEANDPASSWORDREGEX.test(username) || !USERNAMEANDPASSWORDREGEX.test(password)) {
+    res.status(400).json({ error: "Invalid username or password" })
+  };
+
+  const userFolder = "./images/" + getHash(username + password);
+
+  if (fs.existsSync(userFolder)) {
+    const userData = JSON.parse(fs.readFileSync(userFolder + "/userData.json", "utf-8"));
+    res.status(200).json({...userData, userFolder: userFolder.replace("./images", "/user-data")});
+  } else {
+    res.status(404).json({ error: "No such user." });
+  }
+  };
+
+export default { login };
