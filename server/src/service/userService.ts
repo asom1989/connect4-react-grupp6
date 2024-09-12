a5b49672b0b7bd6616604cdd176c8b28602364a6
Staffan Björk <staffanbjork86@gmail.com>
2024-09-12 20:08:05 +0200

    Backend - userService, register function added

--- server/src/service/userService.ts
+++ server/src/service/userService.ts
@@ -28,6 +28,24 @@ const login = (req: Request, res: Response) => {
   } else {
     res.status(404).json({ error: "No such user." });
   }
-  };
+};
 
-export default { login };
+const register = async (req: Request, res: Response) => {
+  const {username, password, encoded}: { username: string; password: string; encoded: string } = req.body;
+  if (!USERNAMEANDPASSWORDREGEX.test(username) || !USERNAMEANDPASSWORDREGEX.test(password)) {
+    res.status(400).json({ error: "Invalid username or password" });
+  }
+
+  const binaryBuffer = Buffer.from(encoded.split("base64")[1], "base64");
+  const userFolder = "./images/" + getHash(username + password);
+  
+  if (fs.existsSync(userFolder)) {
+    res.status(400).json({ error: "User already exists!" });
+  } else {
+    fs.mkdirSync(userFolder);
+    fs.writeFileSync(userFolder + "/userProfileImage.jpg", binaryBuffer);
+    fs.writeFileSync(userFolder + "/userData.json", JSON.stringify({ username }, null, " "), "utf-8");
+    res.status(201).json({ message: "User registered!" });
+  }
+};
+export default { login, register };
