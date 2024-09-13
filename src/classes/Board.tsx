14e7c038f943a7707c1c0c179fae4a3743edb7f8
Linus Tropp <linustropp@gmail.com>
2024-09-13 13:01:49 +0200

    fixed bug :D

--- src/classes/Board.tsx
+++ src/classes/Board.tsx
@@ -56,6 +56,8 @@ export default class Board extends React.Component<
   resetGame = () => {
     this.moves = new Moves();
     this.victoryChecker = new VictoryChecker();
+    this.playerOne.playerMovesMade = 0;
+    this.playerTwo.playerMovesMade = 0;
     this.setState({
       matrix: this.initializeMatrix(),
       currentPlayer: this.playerOne,
@@ -86,9 +88,9 @@ export default class Board extends React.Component<
       currentPlayer.color,
       columnIndex
     );
-
     // Track the last move (row and column)
     const lastMove = this.moves.lastMove;
+    currentPlayer.incrementMoves();
 
     this.setState({ matrix: newMatrix, lastMove }, () => {
       this.victoryChecker.checkForWin(
@@ -98,33 +100,23 @@ export default class Board extends React.Component<
         currentPlayer.color
       );
 
-      this.setState({ matrix: newMatrix }, () => {
-        this.victoryChecker.checkForWin(
-          newMatrix,
-          this.moves.lastMove,
-          this.moves.movesMade,
-          currentPlayer.color
-        );
-
-        if (this.victoryChecker.isGameOver) {
-          const winningCells = this.victoryChecker.winningCells;
-          if (this.victoryChecker.isDraw) {
-            // toast.info("The game is a draw!");
-            this.setState({ winner: "Draw" });
-          } else {
-            // toast.success(`${currentPlayer.name} has won the game!`);
-            this.setState({
-              winner: currentPlayer.name, winningCells,
-              winnerAvatar: currentPlayer.avatar,
-              
-            });
-          }
-          return;
+      if (this.victoryChecker.isGameOver) {
+        const winningCells=this.victoryChecker.winningCells
+        if (this.victoryChecker.isDraw) {
+          // toast.info("The game is a draw!");
+          this.setState({ winner: "Draw" });
+        } else {
+          // toast.success(${currentPlayer.name} has won the game!);
+          this.setState({
+            winner: currentPlayer.name,
+            winnerAvatar: currentPlayer.avatar,
+            winningCells
+          });
+          this.updateLocalStorage(currentPlayer.name);
         }
         return;
-      });
-
-      this.setState(
+      }
+this.setState(
         {
           currentPlayer:
             currentPlayer === this.playerOne ? this.playerTwo : this.playerOne,
@@ -149,6 +141,31 @@ export default class Board extends React.Component<
       );
     });
   };
+  updateLocalStorage(winnerName: string) {
+    const playerStatsString = localStorage.getItem("playerStats");
+    const playerStats: {
+      [key: string]: { wins: number; moves: number; avatar: string };
+    } = playerStatsString ? JSON.parse(playerStatsString) : {};
+
+    if (!playerStats[winnerName]) {
+      playerStats[winnerName] = {
+        wins: 0,
+        moves: Number.MAX_VALUE,
+        avatar: this.state.currentPlayer.avatar,
+      };
+    }
+
+    playerStats[winnerName].wins += 1;
+
+    if (
+      this.state.currentPlayer.playerMovesMade < playerStats[winnerName].moves
+    ) {
+      playerStats[winnerName].moves = this.state.currentPlayer.playerMovesMade;
+    }
+
+    localStorage.setItem("playerStats", JSON.stringify(playerStats));
+  }
+      
 
   render() {
     return (
