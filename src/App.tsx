cad920d32104610c77cef07c56e4dccef9159005
asom1989 <asom.1989@yahoo.com>
2024-09-13 12:48:24 +0200

    WinnerStats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
-import GameSetup from "./components/GameSetup/GameSetup";
-import { Setup } from "./types/types";
-import Board from "./classes/Board";
-import { ToastContainer } from "react-toastify";
+// import { useState } from "react";
+// import GameSetup from "./components/GameSetup/GameSetup";
+// import { Setup } from "./types/types";
+// import Board from "./classes/Board";
+// import { ToastContainer } from "react-toastify";
+import WinnerStats from "./components/WinnerStats/WinnerStats";
 
 export default function App() {
-  const [gameState, setGameState] = useState<Setup | null>(null);
-  const handleGameState = (setup: Setup) => {
-    setGameState(setup);
-  };
+  // const [gameState, setGameState] = useState<Setup | null>(null);
+  // const handleGameState = (setup: Setup) => {
+  //   setGameState(setup);
+  // };
 
-  const handleQuit = () => {
-    setGameState(null);
-  };
+  // const handleQuit = () => {
+  //   setGameState(null);
+  // };
 
   return (
     <div>
-      <ToastContainer theme="colored" position="top-center" autoClose={1700} />
+      {/* <ToastContainer theme="colored" position="top-center" autoClose={1700} />
       {gameState ? (
         <Board onQuit={handleQuit} gameState={gameState} />
       ) : (
         <GameSetup setGameState={handleGameState} />
-      )}
+      )} */}
+      <WinnerStats />
     </div>
   );
 }
