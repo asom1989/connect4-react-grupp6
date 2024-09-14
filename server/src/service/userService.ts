c7dc30e4485eccf81dd981589c6464de51cf3ad8
Staffan Björk <staffanbjork86@gmail.com>
2024-09-14 21:28:00 +0200

    userService - login function updated, now only return data from userData.json

--- server/src/service/userService.ts
+++ server/src/service/userService.ts
@@ -20,14 +20,15 @@ function getHash(userData: string) {
 const login = (req: Request, res: Response) => {
   const { username, password } = req.body;
   if (!CREDENTIALS_REGEX.test(username) || !CREDENTIALS_REGEX.test(password)) {
-    res.status(400).json({ error: "Invalid username or password" })
+    res.status(400).json({ error: "Invalid username or password format" })
   };
 
-  const userFolder = "./images/" + getHash(username + password);
+  const userFolder = USER_FOLDER + getHash(username + password);
 
   if (fs.existsSync(userFolder)) {
     const userData = JSON.parse(fs.readFileSync(userFolder + "/userData.json", "utf-8"));
-    res.status(200).json({...userData, userFolder: userFolder.replace("./images", "/user-data")});
+    res.status(200).json(userData)
+    //res.status(200).json({...userData, userFolder: userFolder.replace("./images", "/user-data")});
   } else {
     res.status(404).json({ error: "No such user." });
   }
