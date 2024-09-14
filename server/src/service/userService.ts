1ebf7186a0e29348a384828645f50fac9abc9c98
Staffan Björk <staffanbjork86@gmail.com>
2024-09-14 20:26:47 +0200

    userService - register function reworked to handle saving images of types jpg/png/gif

--- server/src/service/userService.ts
+++ server/src/service/userService.ts
@@ -4,7 +4,8 @@ import { Request, Response } from "express";
 
 
 const SALT = "2cUMi+VRsGjxqank5yixHA==";
-const USERNAMEANDPASSWORDREGEX = /\w{1,15}/;
+const USERNAMEANDPASSWORDREGEX = /\w{1,20}/;
+const FILE_EXT_REGEX = /^data:image\/(png|jpg|jpeg|gif);base64,(.+)$/;
 
 function getHash(userData: string) {
   const hash = crypto
@@ -31,21 +32,25 @@ const login = (req: Request, res: Response) => {
 };
 
 const register = async (req: Request, res: Response) => {
-  const {username, password, encoded}: { username: string; password: string; encoded: string } = req.body;
+  const { username, password, encoded }: { username: string; password: string; encoded: string } = req.body;
   if (!USERNAMEANDPASSWORDREGEX.test(username) || !USERNAMEANDPASSWORDREGEX.test(password)) {
     res.status(400).json({ error: "Invalid username or password" });
   }
 
-  const binaryBuffer = Buffer.from(encoded.split("base64")[1], "base64");
   const userFolder = "./images/" + getHash(username + password);
+  if (fs.existsSync(userFolder)) { res.status(400).json({ error: "User already exists!" })}
   
-  if (fs.existsSync(userFolder)) {
-    res.status(400).json({ error: "User already exists!" });
-  } else {
-    fs.mkdirSync(userFolder);
-    fs.writeFileSync(userFolder + "/userProfileImage.jpg", binaryBuffer);
-    fs.writeFileSync(userFolder + "/userData.json", JSON.stringify({ username }, null, " "), "utf-8");
-    res.status(201).json({ message: "User registered!" });
+  fs.mkdirSync(userFolder);
+  let userProfileImage = null;
+
+  const imageData = encoded.match(FILE_EXT_REGEX);
+  if (imageData) {
+    const binaryBuffer = Buffer.from(imageData[2], "base64");
+    userProfileImage = `/userProfileImage.${imageData[1]}`;
+    fs.writeFileSync(userFolder + userProfileImage, binaryBuffer);
   }
+
+  fs.writeFileSync(userFolder + "/userData.json", JSON.stringify({ username, userProfileImage }, null, " "), "utf-8");
+  res.status(201).json({ message: "User registered!" });
 };
 export default { login, register };
